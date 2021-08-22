b61aea3bbf17c37eec2c3cf22abd305d8cca0055
sancia15 <59640268+sancia15@users.noreply.github.com>
2021-08-22 10:32:20 +0530

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CF.WorkOrderProcess.Variables
+{
+    class Constants
+    {
+        public class AutonumberConfigurationEntity
+        {
+            public const string rj_name = "rj_name";
+            public const string rj_prefix = "rj_prefix";
+            public const string rj_seperator = "rj_seperator";
+            public const string rj_counter = "rj_counter";
+            public const string rj_autonumberfield = "rj_autonumberfield";
+            public const string rj_entityname = "rj_entityname";
+            public const string rj_incrementby = "rj_incrementby";
+
+
+        }
+        public class Entities
+        {
+           
+            public const string rj_autonumberingconfiguration = "rj_autonumberingconfiguration";
+        }
+
+        public class Operations
+        {
+            public const string create = "create";
+            //public const string update = "update";
+            //public const string delete = "delete";
+        }
+    }
+}
